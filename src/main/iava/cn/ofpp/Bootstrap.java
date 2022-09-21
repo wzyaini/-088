a811143aecc6c1462a5c7652ea8aca70036a39e6
wzyaini <114055111+wzyaini@users.noreply.github.com>
2022-09-21 17:09:59 +0800

    Add files via upload

---
+++ src/main/iava/cn/ofpp/Bootstrap.java
@@ -0,0 +1,38 @@
+package cn.ofpp;
+
+import cn.hutool.core.util.StrUtil;
+import cn.ofpp.core.Wx;
+
+/**
+ * 引导配置
+ *
+ * @author DokiYolo
+ * Date 2022-08-22
+ */
+public class Bootstrap {
+
+    /**
+     * 公众号AppID
+     */
+    public static final String APP_ID = ""wxac7bbe9a5f8e0bd3;
+    /**
+     * 公众号秘钥
+     */
+    public static final String SECRET = "318a6f16cdf77909546ad3eb0502a403";
+
+    /**
+     * 全局模板ID  也可针对单个Friend指定模板
+     */
+    public static final String TEMPLATE_ID = "Y8yFXok2ybVLS0Ren-4mMZVXihip9mx23Qrf4HIyvww";
+
+    /**
+     * 初始化
+     */
+    public static void init() {
+        if (StrUtil.hasEmpty(APP_ID, SECRET, TEMPLATE_ID)) {
+            throw new IllegalArgumentException("请检查配置");
+        }
+        Wx.init();
+    }
+
+}
